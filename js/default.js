7d2966e74013a91aea785528b7838bb50d88f6a7
aaronwwf <dcswuw@gmail.com>
2016-05-10 09:43:33 +0800

    add article list

--- js/default.js
+++ js/default.js
@@ -6,19 +6,26 @@ GRM.addArticle=function(article){
 	this.articleMap[article.name]=article;
 	article.renderWell($("#grid"));
 }
+
 GRM.showArticle=function(article){
-	$("#grid").hide();
-	article.render($("#article"));
+	$("#list").hide();
+	article.renderSider($("#sider"));
+	article.showCommit();
 	$("#article").show();
 }
 GRM.showHome=function(){
 	$("#article").hide();
-	$("#grid").show();
+	$("#list").show();
 }
 
 function loadSettings(callback){
 	$.get("conf/settings.yml", function (data) {
 		GRM.settings = jsyaml.load(data);
+		if(GRM.settings.type=="simple"){
+			GRM.base_url=GRM.settings.server+"/articles/";
+		}else{
+			GRM.base_url="articles/"
+		}		
 		callback();
 	});
 }
@@ -26,15 +33,9 @@ function loadSettings(callback){
 function loadArticles(){
 	var articleNames =  GRM.settings.articles;
 	// for each article, get full info and rendering
-	var base_url="";
-	if(GRM.settings.type=="simple"){
-		base_url=GRM.settings.server+"/articles/";
-	}else{
-		base_url="articles/"
-	}
 	$(articleNames).each(function (i, name) {
 		var folderName = name.replace("/",":");
-	 	$.get(base_url+folderName+"/meta.yml", function (data) {
+	 	$.get(GRM.base_url+folderName+"/meta.yml", function (data) {
 	 		var articleMeta = jsyaml.load(data);
 	 		articleMeta.name=name;
 	 		var article = new Article(articleMeta)
@@ -42,12 +43,13 @@ function loadArticles(){
 	 	});
 	});
 }
+
 //Article Class 
 var Article  = function(meta){
 	var me = this;
 	me.user= meta.name.split("/")[0]
 	me.name= meta.name.split("/")[1];
-	me.fullName = meta.name;
+	me.folderName = meta.name.replace("/",":");
 	$.each(meta.forks,function(index,fork){
 		if (fork.owner == me.user){
 			me.description = fork.description;
@@ -55,6 +57,14 @@ var Article  = function(meta){
 			me.watches = fork.watches;
 		}
 	});
+	$.each(meta.commits,function(index,commit){
+		commit.shortId = commit.id.substring(0,10);
+		commit.shortDate = commit.date.substring(0,10);
+	});
+	me.commits = meta.commits;
+	me.forks = meta.forks;
+	me.$content=$("#content");
+	me.$sider=$("#sider");
 }
 
 
@@ -71,12 +81,50 @@ Article.prototype={
 			return false;
 		});
 	},
-	render:function($container){
-		this.$el= $(Mustache.render($('#article-template').html(),this));
-		$container.append(this.$el);
-		this.bind();
+	renderSider:function(){
+		var me = this;
+		me.$sider.empty();
+		$.each(this.commits,function(index,commit){
+			commit.$el= $(Mustache.render($('#article-sider-template').html(),commit));
+			me.$sider.append(commit.$el);
+		});
+	    this.bindSider();
 	},
-	bind:function(){
+	bindSider:function(){
+		var me =this;
+		$.each(this.commits,function(index,commit){
+			commit.$el.find(".commit").click(function(){
+				me.showCommit(commit);
+				return false;
+			});
+		});
+	},
+	showCommit:function(commit){
+		if(commit==undefined){
+			commit = this.commits[0];
+		}
+		this.render(commit);
+	},
+	render:function(commit){
+		var me = this;
+		var model ={};
+		model.article =this;
+		model.commit=commit;
+		if(commit.content){
+			r();
+		}else{
+			$.get(GRM.base_url+me.folderName+"/"+commit.id+".md",function(data){
+				commit.content=markdown.toHTML(data);
+				r();
+			});
+		}
+		function r(){
+			commit.$el= $(Mustache.render($('#article-template').html(),model));
+			me.$content.html(commit.$el);
+			me.bind(commit);
+		};
+	},
+	bind:function(commit){
 		return;
 	}
 }
@@ -84,6 +132,7 @@ Article.prototype={
 $(document).ready(function(){
 	loadSettings(function(){
 		loadArticles();
+		GRM.showHome();
 	})
 
 });
@@ -101,4 +150,8 @@ $("#theme-select").find("a").click(function(){
 	$(this).parent().addClass("active");
 	$(document).trigger("click");
 	return false;
-})
+});
+$("#brand").click(function(){
+	GRM.showHome();
+	return false;
+});
